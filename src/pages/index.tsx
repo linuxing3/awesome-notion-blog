503ca4d41d6db76107f90f548923b1c6cb1bf0d2
wenju xing <linuxing3@qq.com>
2021-03-18 07:54:25 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
           width="250"
           alt="Vercel + Notion"
         />
-        <h1>My Notion Blog</h1>
+        <h1>Awesome Notion Blog</h1>
         <h2>
           Blazing Fast Notion Blog with Next.js'{' '}
           <ExtLink
